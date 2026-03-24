e122f3fed708e4c6f42c8d392d00d464c44b8244
Nelson Durrant <snelsondurrant@gmail.com>
2026-03-24 13:43:38 -0600

    Fix for evo numpy deps

--- evals/evo_bags.sh
+++ evals/evo_bags.sh
@@ -92,6 +92,7 @@ for bag_path in ${bags_to_eval}; do
   done
 done
 
-python3 $(dirname "$0")/plots/trajectory_plot.py
-python3 $(dirname "$0")/plots/timing_plot.py
-python3 $(dirname "$0")/plots/benchmark_plot.py
+evo_python="${HOME}/.local/pipx/venvs/evo/bin/python"
+${evo_python} $(dirname "$0")/plots/trajectory_plot.py
+${evo_python} $(dirname "$0")/plots/timing_plot.py
+${evo_python} $(dirname "$0")/plots/benchmark_plot.py
