bb5de788a4f929c35556d409efe5987c948a5fc2
Nelson Durrant <snelsondurrant@gmail.com>
2026-06-11 16:42:47 -0600

    Added coug_fgo_base launch

---
+++ coug_fgo/launch/coug_fgo_base.launch.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2026 BYU FROST Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import (
+    EnvironmentVariable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+
+
+def launch_setup(context, *args, **kwargs) -> list:
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    agent_list_str = LaunchConfiguration("agent_list").perform(context)
+
+    agent_namespaces = yaml.safe_load(agent_list_str)
+    auv_ns = agent_namespaces[0]
+
+    fleet_params = PathJoinSubstitution(
+        [
+            EnvironmentVariable("CONFIG_DIR"),
+            "fleet",
+            "coug_fgo_params.yaml",
+        ]
+    )
+    auv_params = PathJoinSubstitution(
+        [
+            EnvironmentVariable("CONFIG_DIR"),
+            f"{auv_ns}_params.yaml",
+        ]
+    )
+
+    return [
+        Node(
+            package="coug_fgo",
+            executable="navsat_odom",
+            name="navsat_odom_node",
+            namespace="base_station",
+            parameters=[
+                fleet_params,
+                auv_params,
+                {
+                    "use_sim_time": use_sim_time,
+                    "map_frame": "map",
+                    "set_origin": True,
+                    "input_topic": f"/{auv_ns}/gps/fix",
+                    "output_topic": "odometry/gps_null",
+                },
+            ],
+        ),
+    ]
+
+
+def generate_launch_description() -> LaunchDescription:
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="false",
+                description="Use simulation/rosbag clock if true",
+            ),
+            DeclareLaunchArgument(
+                "agent_list",
+                default_value="[auv0]",
+                description=(
+                    "YAML list of AUV namespaces "
+                    "(e.g. '[coug1sim]' or '[coug1sim, coug2sim]')"
+                ),
+            ),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
